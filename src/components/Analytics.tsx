3ed68b307eab5fda91a650e696ee157b37849131
Vebdev Admin <vebdev@agency.com>
2026-03-15 13:57:31 +0400

    fix: restore JSX balance and total animation stabilization for Analytics section

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -100,6 +100,7 @@ export default function Analytics() {
                     {w}
                   </motion.button>
                 ))}
+              </div>
             </div>
           </div>
         </div>
